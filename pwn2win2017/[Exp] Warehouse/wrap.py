bdd0d9a485d2039f9db2008b0f2e050fd3ff6bf7
pedroysb <pedroyossis@gmail.com>
2018-11-30 10:10:33 +0100

    Delete wrap.py

--- pwn2win2017/[Exp] Warehouse/wrap.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import sys
-from os import system
-
-print('Welcome to the Warehouse.')
-print('Tell me where and what to store. Exit with ".".')
-sys.stdout.flush()
-system("/home/warehouse/warehouse")
-print("Your stuffs were stored! Bye...")
